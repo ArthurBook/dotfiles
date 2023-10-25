36c24c7f59da1cc7809674f123d01ddd76b6fe49
Arthur <arthur@Arthurs-MacBook-Air.local>
2023-10-25 11:01:34 -0700

    updates

--- init.lua
+++ init.lua
@@ -18,25 +18,10 @@ require('packer').startup(function()
   use 'davidhalter/jedi-vim'
   use 'psf/black'
   use 'ycm-core/YouCompleteMe'
-  use 'scrooloose/nerdtree'
   use 'sheerun/vim-polyglot'
-  use {
-    'dense-analysis/ale',
-    config = function()
-      vim.g.ale_linters = {python = {'flake8', 'pylint', 'mypy'}}
-      vim.g.ale_fixers = {python = {'autopep8', 'yapf', 'black'}}
-      vim.api.nvim_set_keymap('n', '<C-l>', ':ALEFix<CR>', {noremap = true, silent = true})
-    end
-  }
-  use {
-    'neovim/nvim-lspconfig',
-    config = function()
-      local lspconfig = require('lspconfig')
-      lspconfig.pyright.setup({
-        cmd = { "pyright-langserver", "--stdio" },
-      })
-    end
-  }
+  use 'scrooloose/nerdtree'
+  use 'dense-analysis/ale'
+  use 'neovim/nvim-lspconfig'
   use 'tpope/vim-fugitive'
   use 'puremourning/vimspector'
   use 'vim-airline/vim-airline'
@@ -48,6 +33,29 @@ require('packer').startup(function()
   }
 end)
 
+-- Defaults
+vim.cmd [[
+  augroup NerdTreeAutoCommands
+    autocmd!
+    autocmd VimEnter * NERDTree | wincmd w
+  augroup END
+
+  function! ToggleNERDTreeFocus()
+    if exists("t:NERDTreeBufName")
+      if bufname('%') == t:NERDTreeBufName
+        wincmd w
+      else
+        NERDTreeFocus
+      endif
+    else
+      NERDTree
+    endif
+  endfunction
+
+  nnoremap <silent> <C-n> :call ToggleNERDTreeFocus()<CR>
+  nnoremap <silent> <C-S-n> :NERDTreeToggle<CR>
+]]
+
 -- Language-Specific Settings
 vim.cmd [[
   augroup MyPythonSettings
@@ -55,12 +63,37 @@ vim.cmd [[
     autocmd BufNewFile,BufRead *.py lua SetupPythonEnv()
     autocmd BufNewFile,BufRead *.py lua SetupPythonVimSettings()
     autocmd BufWritePre *.py execute ':Black'
+    autocmd BufNewFile,BufRead *.py lua SetupALEForPython()
+    autocmd BufNewFile,BufRead *.py lua SetupLSPForPython()
   augroup END
 ]]
 
 function SetupPythonEnv()
-  vim.g.python3_host_prog = vim.fn.system('pyenv which python'):gsub('%s+$', '')
-  vim.g.ycm_python_binary_path = vim.fn.system('poetry env info -p'):gsub('%s+$', '')
+  local pyenv_path = vim.fn.system('pyenv which python'):gsub('%s+$', '')
+  local poetry_path = vim.fn.system('poetry env info -p'):gsub('%s+$', '')
+  
+  -- Use pyenv for python3_host_prog
+  vim.g.python3_host_prog = pyenv_path
+  
+  -- Check if poetry exists, otherwise fallback to pyenv or system python
+  if vim.fn.empty(poetry_path) == 1 then
+    vim.g.ycm_python_binary_path = vim.fn.system('pyenv which python'):gsub('%s+$', '')
+  else
+    vim.g.ycm_python_binary_path = poetry_path
+  end
+end
+
+function SetupLSPForPython()
+  local lspconfig = require('lspconfig')
+  lspconfig.pyright.setup({
+    cmd = { "pyright-langserver", "--stdio" },
+  })
+end
+
+function SetupALEForPython()
+  vim.g.ale_linters = {python = {'flake8', 'pylint', 'mypy'}}
+  vim.g.ale_fixers = {python = {'autopep8', 'yapf', 'black'}}
+  vim.g.ale_enabled = 1
 end
 
 function SetupPythonVimSettings()
@@ -72,7 +105,3 @@ function SetupPythonVimSettings()
   vim.opt.autoindent = true
   vim.cmd 'set fileformat=unix'
 end
-
--- Keybindings
-vim.api.nvim_set_keymap('n', '<C-n>', ':NERDTreeToggle<CR>', {noremap = true, silent = true})
-
