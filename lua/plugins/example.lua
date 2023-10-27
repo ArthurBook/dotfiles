96ac6eb4ec46df3ea349fb947405444c8df90bfa
Arthur <atte.book@gmail.com>
2023-10-27 12:08:46 -0700

    config base working

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
--- if true then return {} end
+if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -10,7 +10,7 @@
 -- * override the configuration of LazyVim plugins
 return {
   -- add gruvbox
-  {'petobens/poet-v' },
+  -- { "petobens/poet-v" },
 
   -- { "ellisonleao/gruvbox.nvim" },
   --
