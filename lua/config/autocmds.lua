e84524b4586a0c35ccff1e6633abcb794d1fc6c7
Arthur <atte.book@gmail.com>
2023-10-27 18:05:42 -0700

    WIP setting stuff up

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,13 +4,26 @@
 -- -- ... existing autocmds
 
 vim.api.nvim_create_autocmd("VimEnter", {
-  desc = "Auto select virtualenv Nvim open",
-  pattern = "*",
-  callback = function()
-    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
-    if venv ~= "" then
-      require("venv-selector").retrieve_from_cache()
-    end
-  end,
-  once = true,
+	desc = "Auto select virtualenv Nvim open",
+	pattern = "*",
+	callback = function()
+		local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+		if venv ~= "" then
+			require("venv-selector").retrieve_from_cache()
+		end
+	end,
+	once = true,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		require("lspconfig").pylsp.setup({
+			filetypes = { "python" },
+			settings = {
+				pylsp = require("lspconfig").pylsp, -- required to enable pylsp
+				configurationSources = { "black", "pylint", "mypy" },
+				formatCommand = { "black" },
+			},
+		})
+	end,
 })
