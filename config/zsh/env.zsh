706d5157044548e7ce28abd6c1b0a73cc4022ba6
Arthur <atte.book@gmail.com>
2023-12-03 12:03:18 -0800

    forgotten "

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -1,5 +1,5 @@
 export PATH="/usr/local/bin:$PATH"
 export PATH="/opt/homebrew/bin:$PATH"
-export PATH="/opt/homebrew/opt/sphinx-doc/bin:$PATH
+export PATH="/opt/homebrew/opt/sphinx-doc/bin:$PATH"
 export PATH="/Users/arthur/.local/share/bob/nvim-bin:$PATH" 
 export HOMEBREW_PREFIX=$(brew --prefix)
