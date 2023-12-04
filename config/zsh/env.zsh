b96cece9d27dd25eb97a9b75ca310500b9d13a9c
Arthur <atte.book@gmail.com>
2023-12-04 09:34:55 -0800

    modular

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -1,5 +1,16 @@
+# binaries
 export PATH="/usr/local/bin:$PATH"
-export PATH="/opt/homebrew/bin:$PATH"
-export PATH="/opt/homebrew/opt/sphinx-doc/bin:$PATH"
-export PATH="/Users/arthur/.local/share/bob/nvim-bin:$PATH" 
+
+# bob (nvim)
+export PATH="~/.local/share/bob/nvim-bin:$PATH" 
+
+# brew
 export HOMEBREW_PREFIX=$(brew --prefix)
+export PATH="/opt/homebrew/bin:$PATH"
+
+# sphinx
+export PATH="/opt/homebrew/opt/sphinx-doc/bin:$PATH"
+
+# mojo
+export MODULAR_HOME="/Users/arthur/.modular"
+export PATH="~/.modular/pkg/packages.modular.com_mojo/bin:$PATH"
