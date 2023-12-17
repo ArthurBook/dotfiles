854795cea48e4e2e780cdb27076b8e2a37390a07
Arthur <atte.book@gmail.com>
2023-12-17 09:21:52 -0800

    env trouble

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -4,13 +4,13 @@ export PATH="/usr/local/bin:$PATH"
 # bob (nvim)
 export PATH="~/.local/share/bob/nvim-bin:$PATH" 
 
-# brew
-export HOMEBREW_PREFIX=$(brew --prefix)
-export PATH="/opt/homebrew/bin:$PATH"
-
 # sphinx
 export PATH="/opt/homebrew/opt/sphinx-doc/bin:$PATH"
 
 # mojo
 export MODULAR_HOME="/Users/arthur/.modular"
 export PATH="~/.modular/pkg/packages.modular.com_mojo/bin:$PATH"
+
+# brew
+export PATH="/opt/homebrew/bin:$PATH"
+export HOMEBREW_PREFIX=$(brew --prefix)
