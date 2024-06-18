2d19e9a50a42cc715bd92f174632b0a35c66dadd
Arthur <atte.book@gmail.com>
2024-06-18 17:43:00 +0300

    discord and Q

--- config/zsh/settings.zsh
+++ config/zsh/settings.zsh
@@ -4,3 +4,8 @@ if [[ -n $SSH_CONNECTION ]]; then
 else
   export EDITOR='nvim'
 fi
+
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
