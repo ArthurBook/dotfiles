be93def374650f236d47191d6e137dba96a120f3
Arthur <atte.book@gmail.com>
2025-04-20 22:14:15 -0700

    feat: add stashes to pretty git stat

--- dot_config/fish/functions/git_summary.fish
+++ dot_config/fish/functions/git_summary.fish
@@ -10,9 +10,26 @@ function git_summary
     set -l color_section (set_color --bold 7aa2f7) # blue
     set -l color_reset (set_color normal)
 
-    echo "$color_section✏️ Files:$color_reset"
-    git status --short --untracked-files --renames
-    echo
+    set -l git_status (git status --short --untracked-files --renames)
+    if test -z "$git_status"
+        set_color --bold a6e3a1 # green
+        echo "✅ Working tree clean. No changes to show."
+        set_color normal
+    else
+        echo "$color_section✏️ Files:$color_reset"
+        echo "$git_status"
+    end
+
+    set -l stash_count (git stash list | wc -l | string trim)
+    if test "$stash_count" -gt 0
+        echo "$color_section📦 Stashes:$color_reset"
+        git stash list | while read -l line
+            set_color --bold cba6f7 # purple
+            echo "• $line"
+            set_color normal
+        end
+    end
+
     echo "$color_section🕘 Log:$color_reset"
     git-graph --max-count 10 --wrap auto --no-pager --style round
 end
