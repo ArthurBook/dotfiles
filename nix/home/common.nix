aa4437c47ca1004c44ab995f25c057574b9f3e5a
Arthur <atte.book@gmail.com>
2025-11-23 12:56:27 -0800

    feat(zellij): add basic zellij zetup

--- nix/home/common.nix
+++ nix/home/common.nix
@@ -4,6 +4,7 @@
   imports = [
     ./shell/fish.nix
     ./shell/starship.nix
+    ./shell/zellij.nix
   ];
 
   home.stateVersion = "24.05";
--- nix/home/shell/fish.nix
+++ nix/home/shell/fish.nix
@@ -6,6 +6,12 @@
     interactiveShellInit = ''
       set -gx PATH $HOME/.local/bin $PATH
       starship init fish | source
+      set -U fish_key_bindings fish_vi_key_bindings
+
+      if set -q ZELLIJ
+      else
+          zellij
+      end
     '';
     shellAliases = {
       ll = "ls -lh";
