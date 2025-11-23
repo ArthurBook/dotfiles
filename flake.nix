f67e3a0a7ebbeb5f132de2bdb58fcc4c2dffa5a1
Arthur <atte.book@gmail.com>
2025-11-23 14:51:23 -0800

    feat(packages): add claude-code

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- nix/home/packages.nix
+++ nix/home/packages.nix
@@ -1,7 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  nixpkgs.config.allowUnfree = true;
+
   home.packages = with pkgs; [
+    claude-code # Agentic coding tool that lives in your terminal
     lazygit # A simple terminal UI for git commands
     ripgrep # Recursively searches directories
     fd # The friendly file finder
