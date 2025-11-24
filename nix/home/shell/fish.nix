aaff7724fab69170b5abadaa800a13881bf3e574
Arthur <atte.book@gmail.com>
2025-11-24 12:31:42 -0800

    feat(shell): add good old opt & cmd navigation

--- nix/home/shell/fish.nix
+++ nix/home/shell/fish.nix
@@ -9,6 +9,15 @@
       direnv hook fish | source
       set -U fish_key_bindings fish_vi_key_bindings
 
+      # good old opt & cmd navigation
+      bind -M insert ctrl-u backward-kill-line
+      bind -M insert ctrl-a beginning-of-buffer
+      bind -M insert ctrl-e end-of-buffer
+      bind -M insert alt-b backward-word
+      bind -M insert alt-f forward-word
+      bind -M insert ctrl-w backward-kill-word # same as alt-backspace
+      bind -M insert alt-backspace backward-kill-word
+
       if set -q ZELLIJ
       else
           zellij
