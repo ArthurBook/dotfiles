c6dbfbfd414098ff5875a9e6ac0a67076674354c
Arthur <atte.book@gmail.com>
2025-11-24 11:26:12 -0800

    feat(packages): add tokyonight theme to eza

--- nix/home/packages.nix
+++ nix/home/packages.nix
@@ -37,6 +37,12 @@
     };
   };
 
+  # Eza Tokyo Night theme
+  home.file.".config/eza/theme.yml".source = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/eza-community/eza-themes/main/themes/tokyonight.yml";
+    sha256 = "0mkiad3jhqy9rdm0sj9gwi46v4i38m9vg8021cwd1qx7003mijh6";
+  };
+
   # Bottom (btm) configuration with Tokyo Night theme
   home.file.".config/bottom/bottom.toml".text = ''
     # Tokyo Night color scheme
